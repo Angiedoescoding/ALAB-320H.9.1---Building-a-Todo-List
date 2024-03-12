67226ae20a225bc46b078a52233bac796243083d
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-12 10:57:08 -1000

    TodoItem skeleton

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import {useState} from 'react'
 
 function TodoItem() {
+
+  const [editing, setEditing] = useState(false);
+  const [editText, setEditText] = useState();
+
   return (
-    <div>TodoItem</div>
-  )
+    // <div>TodoItem</div>
+    <li>
+      {editing ? (<input
+          type="text" value={editText} onChange={handleTextChange} />) : (
+            <div>
+              <input
+                type="text" 
+                checked={completed} 
+                onChange={function(){
+                  toggleComplete;
+                }} />
+            </div>
+          )}
+          <button>
+            Edit
+          </button>
+          <button>
+            Delete
+          </button>
+    </li>
+  );
 }
 
 export default TodoItem
