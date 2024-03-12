50d3d17fe5542796d7cd57b12f2da1fe57b6cf35
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-12 10:29:16 -1000

    TodoList skeleton

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import {useReducer} from 'react'
 import TodoItem from './TodoItem'
 
+
+function listReducer() {
+    switch () {
+        case add:
+            return ;
+
+        case remove:
+            return ;
+    
+        case complete:
+            return ;
+        
+        case edit:
+            return ;
+        
+    }
+}
+
+
 function TodoList() {
-  return (
-    <div>TodoList
-        <TodoItem />
-    </div>
-  )
+    
+    
+
+
+    return (
+        <div>
+            <h1>Create To Do List</h1>
+            <input 
+                type="text"
+                placeholder="Add a task"/>
+            <ul>
+                <TodoItem />
+            </ul>
+        </div>
+    );
 }
 
 export default TodoList
